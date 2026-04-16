d78b8a9d9743a6be8523c2cb1b0ac2d30c98b613
Daeki <kimdaeki@gmail.com>
2026-04-16 18:07:32 +0900

    board2

--- src/test/java/com/winter/app/board/qna/QnaMapperTest.java
+++ src/test/java/com/winter/app/board/qna/QnaMapperTest.java
@@ -16,6 +16,23 @@ class QnaMapperTest {
 	private QnaMapper qnaMapper;
 	
 	@Test
+	void testCreate() throws Exception{
+		for(int i=0;i<23;i++) {
+			QnaDTO qnaDTO = new QnaDTO();
+			qnaDTO.setBoardTitle("qna title "+i);
+			qnaDTO.setBoardWriter("qna writer "+i);
+			qnaDTO.setBoardContents("qna contents "+i);
+			qnaMapper.create(qnaDTO);
+			if(i%3==0) {
+				Thread.sleep(500);
+			}
+		}
+		
+		System.out.println("finish");
+	}
+	
+	
+	@Test
 	void testList() throws Exception {
 		List<BoardDTO> ar = qnaMapper.list();
 		assertNotEquals(0, ar.size());
